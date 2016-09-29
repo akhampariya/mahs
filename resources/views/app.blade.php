9c3e6d9b240cf0dc33bec3f40f1b598bb1fd9aa6
ejalba <ealba@unomaha.edu>
2016-09-29 15:18:46 -0500

    improved

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,11 +7,11 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 
 </head>
-<body background="http://localhost/efs/public/saya.png">
+<body background="http://financeblog.herokuapp.com/saya.png">
 <div class="container">
     <div style="text-align: center;">
         <br>
-    <a href="index.php">Home Page</a> |
+    <a href="https://financeblog.herokuapp.com/">Home Page</a> |
     <a href="{{ action('CustomerController@index') }}">Customers</a> |
     <a href="{{ action('StockController@index') }}">Stocks</a> |
     <a href="{{ action('InvestmentController@index') }}">Investments</a>
