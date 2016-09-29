509c641f9dbf0ef89498460e29d49205070b185a
ejalba <ealba@unomaha.edu>
2016-09-29 14:21:42 -0500

    make it better

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,7 +11,7 @@
 <div class="container">
     <div style="text-align: center;">
         <br>
-    <a href="http://localhost/efs/public/">Home Page</a> |
+    <a href="index.php">Home Page</a> |
     <a href="{{ action('CustomerController@index') }}">Customers</a> |
     <a href="{{ action('StockController@index') }}">Stocks</a> |
     <a href="{{ action('InvestmentController@index') }}">Investments</a>
