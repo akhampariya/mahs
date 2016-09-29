a9d3f887a36271e12df5a8e0080b63faf39bd359
ejalba <ealba@unomaha.edu>
2016-09-29 18:54:06 -0500

    nooooooooo!

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
 
+namespace App\Http\Controllers;
+set_include_path(get_include_path() . PATH_SEPARATOR . 'localhost\efs\google-api-php-client\src');
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,4 +11,5 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
 }
