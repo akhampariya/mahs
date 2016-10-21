1ca1d3d1235b7f2f1b23c254340f1c150eb54f25
AJ <akhampariya@unomaha.edu>
2016-10-21 01:25:10 -0500

    new blog

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,9 +6,9 @@
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
                 <div class="panel-heading">Welcome to MAH</div>
-                <div style="text-align: center;"><img src="images/UNO-Mav.png"></div>
+                <!-- <div style="text-align: center;"><img src="images/UNO-Mav.png"></div> -->
                 <div class="panel-body">
-                    Application description to be filled.
+                    <h2> Application description to be filled.</h2>
                 </div>
             </div>
         </div>
