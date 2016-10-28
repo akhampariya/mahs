220c108658ee5f25595db2ba17f23679396a3d53
AJ <akhampariya@unomaha.edu>
2016-10-28 18:50:35 -0500

    fixed sign up issue

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,8 +6,23 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard - Role based landing page</div>
                 <div class="panel-body">
-                    You are logged in!
-                </div>
+                 You are logged in!
+                
+				
+				     @if (Auth::check())
+                    <!-- Left Side Of Navbar -->
+                @role('admin')
+                <h1> You are logged in admin <h2>
+				@endrole
+				@role('pmanager')
+           <h1> You are logged in pmgr <h2>
+				@endrole
+            @role('bmanager')
+           <h1> You are logged in bmgr <h2>
+				@endrole
+            
+			@endif			
+				</div>
             </div>
 
         </div>
