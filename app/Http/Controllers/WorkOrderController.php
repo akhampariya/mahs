39a91ecf584a615043da806fbe80579ead2990dc
akhampariya <akhampariya@unomaha.edu>
2016-11-16 11:49:11 -0600

    Update WorkOrderController.php
    sorry for direct kick

--- app/Http/Controllers/WorkOrderController.php
+++ app/Http/Controllers/WorkOrderController.php
@@ -1,7 +1,8 @@
 <?php
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
-//use App\Http\Requests;
+use App\Http\Requests;
+use App\Http\Requests\UserRequest;
 use App\Workorder;
 //use Auth;
 //use Session;
