be7de1c245d36c1c2ed2c8239b1c7ad3fd54415e
akhampariya <akhampariya@unomaha.edu>
2016-11-16 18:59:49 -0600

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,12 +6,9 @@
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
                 <div class="panel-heading">Welcome to Mercy Affordable Housing Inc.</div>
-                <div style="text-align: center;"><img src="images/MAHLOGO.png"></div> 
+                <div style="text-align: center;"><img src="images/MAHLOGO.png" class="img" /></div> 
                 <div class="panel-body">
                 <!-- <div style="text-align: center;"><img src="images/UNO-Mav.png"></div> -->
-                
-
-	       
 <p><strong> Mercy Affordable Housing Inc.</strong> </p>
       <p> We make housing affordable for every one </p>
 				</div>
